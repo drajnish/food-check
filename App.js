14aa014f497477289d71d8335ba67aa4158dc81b
Rajnish Deshmukh <rajnishdeshmukh05@gmail.com>
2023-01-23 22:04:43 +0530

    initial commit for food check app

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return (
+    <>
+      <h1>This is a Food Check App</h1>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
+
+export default App;
